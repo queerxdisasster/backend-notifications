366e2c37f0841656c51406bab3207846097af0b8
queerxdisasster <7germania7@gmail.com>
2025-02-05 22:32:40 +0300

    123

---
+++ src/main/java/com/queerxdisasster/backendnotifications/BackendNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.queerxdisasster.backendnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendNotificationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/queerxdisasster/backendnotifications/BackendNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queerxdisasster.backendnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
